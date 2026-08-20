84d7f542f4edba481ee0db5475f4e546be43eb05
pratikmardi <pratikmardi@gmail.com>
2026-08-20 08:46:54 +0530

    Change MainActivity to extend Activity instead of ComponentActivity

--- app/src/main/java/com/pratikmardi/galaxystrike/Mainactivity.kt
+++ app/src/main/java/com/pratikmardi/galaxystrike/Mainactivity.kt
@@ -1,9 +1,9 @@
 package com.pratikmardi.galaxystrike
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
